35452867433b4b0a8e6496573b1cfb450c17e1db
ansaryergesh <yergeshovansar@gmail.com>
2020-03-18 01:51:21 +0600

    main index

--- src/index.js
+++ src/index.js
@@ -7,6 +7,26 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
+const generateId = () => parseInt(Math.random() *1000);
+const initialState = {
+    books: [
+        {
+          id: generateId(),
+          title: 'Thor Ragnarok',
+          category: 'Action',
+        },
+        {
+          id: generateId(),
+          title: 'IT',
+          category: 'Horror',
+        },
+        {
+          id: generateId(),
+          title: 'Tokio',
+          category: 'Horror',
+        },
+    ],
+}
 const store = createStore(rootReducer,initialState);
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
