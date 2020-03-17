142c7d9bec7ecfcd4c893c5706a03da3acfc380d
ansaryergesh <yergeshovansar@gmail.com>
2020-03-17 23:10:05 +0600

    booklist

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
-
+import BooksForm from  '../containers/BooksForm';
+import BooksList from '../containers/BooksList';
 function App() {
   return (
     <div className="App">
-      <p>Milestone1. Basic Setup</p>
+      <BooksList />
+      <BooksForm />
     </div>
   );
 }
---
+++ src/containers/BooksList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Book from '../components/Books';
+
+const BookList = ({books}) => {
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Category</th>
+            </tr>
+        </thead>
+        <tbody>
+            {books.map(book => (
+                <Book key ={book.id} book= {book} />
+            ))}
+        </tbody>
+    </table>
+};
+
+BookList.defaultPropTypes = {
+    books: [],
+}
+
+const mapStateToProps = state => ({books: state.books});
+
+export default connect(mapStateToProps)(BookList);
